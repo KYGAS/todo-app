767f9b87202b048a708e7ddf07c353f229475ca1
aca <aca@stuntcoders.com>
2022-08-29 16:01:53 +0200

    Refactor part of HTML building into a variable

--- web/src/layouts/PageHeaderLayout/PageHeaderLayout.js
+++ web/src/layouts/PageHeaderLayout/PageHeaderLayout.js
@@ -4,19 +4,21 @@ import { Link, routes } from "@redwoodjs/router"
 const PageHeaderLayout = ({ children }) => {
   const { isAuthenticated, currentUser, logOut } = useAuth()
 
+  let isLoggedIn = isAuthenticated ? (
+    <div>
+      <span>Logged in as {currentUser.id}</span>{' '}
+      <button type="button" onClick={logOut}>
+        Logout
+      </button>
+    </div>
+  ) : (
+    <Link to={routes.login()}>Login</Link>
+  )
+
   return (
   <>
       <h1><Link to={routes.home()}>To-Do WebApp</Link>
-        {isAuthenticated ? (
-          <div>
-            <span>Logged in as {currentUser.email}</span>{' '}
-            <button type="button" onClick={logOut}>
-              Logout
-            </button>
-          </div>
-        ) : (
-          <Link to={routes.login()}>Login</Link>
-        )}
+        {isLoggedIn}
       </h1>
       <hr></hr>
       <main>
