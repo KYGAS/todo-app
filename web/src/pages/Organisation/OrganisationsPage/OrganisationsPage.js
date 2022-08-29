ab12c25750b40154606fc8edf1712e40654a5080
aca <aca@stuntcoders.com>
2022-08-29 16:55:21 +0200

    Add currentUserId parameter to companies page

--- web/src/pages/Organisation/OrganisationsPage/OrganisationsPage.js
+++ web/src/pages/Organisation/OrganisationsPage/OrganisationsPage.js
@@ -1,6 +1,6 @@
 import OrganisationsCell from 'src/components/Organisation/OrganisationsCell'
 
-const OrganisationsPage = () => {
+const OrganisationsPage = ({id}) => {
   return <OrganisationsCell />
 }
 
