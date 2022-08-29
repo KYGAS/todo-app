092479339213e997eb9448812f2d21499b37175e
aca <aca@stuntcoders.com>
2022-08-29 15:45:01 +0200

    Add sign in/log out to page layout

--- web/src/layouts/PageHeaderLayout/PageHeaderLayout.js
+++ web/src/layouts/PageHeaderLayout/PageHeaderLayout.js
@@ -1,9 +1,23 @@
+import { useAuth } from "@redwoodjs/auth"
 import { Link, routes } from "@redwoodjs/router"
 
 const PageHeaderLayout = ({ children }) => {
+  const { isAuthenticated, currentUser, logOut } = useAuth()
+
   return (
   <>
-      <h1><Link to={routes.home()}>To-Do WebApp</Link></h1>
+      <h1><Link to={routes.home()}>To-Do WebApp</Link>
+        {isAuthenticated ? (
+          <div>
+            <span>Logged in as {currentUser.email}</span>{' '}
+            <button type="button" onClick={logOut}>
+              Logout
+            </button>
+          </div>
+        ) : (
+          <Link to={routes.login()}>Login</Link>
+        )}
+      </h1>
       <hr></hr>
       <main>
         {children}
