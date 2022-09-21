5343c5dee4a6c86b698daa3fc648586e996d37c2
aca <aca@stuntcoders.com>
2022-09-21 14:46:02 +0200

    Pass necessary input to NewMessage page

--- web/src/pages/Message/NewMessagePage/NewMessagePage.js
+++ web/src/pages/Message/NewMessagePage/NewMessagePage.js
@@ -1,7 +1,7 @@
 import NewMessage from 'src/components/Message/NewMessage'
 
-const NewMessagePage = () => {
-  return <NewMessage />
+const NewMessagePage = ({id}) => {
+  return <NewMessage id={id} />
 }
 
 export default NewMessagePage
